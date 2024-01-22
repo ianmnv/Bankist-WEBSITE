17e4f002ef9b7c1c4a631be50a78ca3681291fed
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-01-21 23:07:30 -0800

    Implemented sticky nav in the old way

--- script.js
+++ script.js
@@ -131,6 +131,17 @@ const handlerHover = function (e) {
 nav.addEventListener('mouseover', handlerHover.bind(0.5));
 nav.addEventListener('mouseout', handlerHover.bind(1));
 
+// --IMPLEMENTING A STICKY NAVIGATION: THE SCROLL EVENT--
+
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+
+  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 ////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
